79853428c99dc1af8055a14a66969a2385a6b646
Niyansai <73218318+Niyansai@users.noreply.github.com>
2021-02-23 16:11:24 +0530

    Initial Commit

---
+++ Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Footer.css";
+import Youtubelogo from "./Images/Youtubelogo.png";
+import Facebooklogo from "./Images/Facebooklogo.png";
+import Pineterestlogo from "./Images/Pineterestlogo.png";
+
+
+
+function Footer() {
+    
+
+    return (
+
+
+<div className="disclaimer">
+           <p>
+           <small>©Website Developed By Sainath K</small>
+           </p>
+         </div>
+);
+}
+
+
+export default Footer;
---
+++ firbase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBIzjQ0u058_sQtwZf1bxg97yCOf5Io9jo",
+    authDomain: "iqcheckquiz.firebaseapp.com",
+    projectId: "iqcheckquiz",
+    storageBucket: "iqcheckquiz.appspot.com",
+    messagingSenderId: "565839117208",
+    appId: "1:565839117208:web:1c6d4c1833619f3f6849ed",
+    measurementId: "G-MGH1EH849R"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
